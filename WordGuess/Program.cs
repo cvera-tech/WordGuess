2624711d26d2a40979b022adce606815c672deb9
jbvera <johnbenedictv@gmail.com>
2019-08-13 15:54:19 -0400

    Add GetPhraseCharacters and GetPhraseDistinctCharacters

--- WordGuess/Program.cs
+++ WordGuess/Program.cs
@@ -12,6 +12,7 @@ namespace WordGuess
         {
             Console.WriteLine("Welcome to the Word Guess app!");
             List<string> phrases = GetPhrases();
+            PlayGame(phrases);
             Console.ReadKey();
         }
 
@@ -41,5 +42,56 @@ namespace WordGuess
             return phrases[randomIndex];
         }
 
+        /// <summary>
+        /// This is the method where the game is played.
+        /// </summary>
+        /// <param name="phrases">The list of phrases to choose from.</param>
+        static void PlayGame(List<string> phrases)
+        {
+            string phrase = SelectPhrase(phrases);
+            char[] phraseCharacters = GetPhraseCharacters(phrase);
+            HashSet<char> distinctCharacters = GetPhraseDistinctCharacters(phraseCharacters);
+
+        }
+
+        /// <summary>
+        /// Returns an array of uppercase characters from the input string.
+        /// </summary>
+        /// <param name="phrase">The phrase to extract characters from.</param>
+        /// <returns>The array of uppercase characters.</returns>
+        static char[] GetPhraseCharacters(string phrase)
+        {
+            char[] phraseCharacters = phrase.ToUpper().ToCharArray();
+            return phraseCharacters;
+        }
+
+        /// <summary>
+        /// Returns a HashSet of distinct letters from an input character array.
+        /// </summary>
+        /// <param name="phraseCharacters">The character array to prune.</param>
+        /// <returns>A HashSet of distinct characters.</returns>
+        static HashSet<char> GetPhraseDistinctCharacters(char[] phraseCharacters)
+        {
+            var distinctCharacters = new HashSet<char>(phraseCharacters);
+            distinctCharacters.RemoveWhere(NotLetter);
+            return distinctCharacters;
+        }
+
+        /// <summary>
+        /// Predicate method that checks whether or not a character is a letter.
+        /// </summary>
+        /// <param name="character">The character to check.</param>
+        /// <returns>True if not a letter; false if a letter.</returns>
+        static bool NotLetter(char character)
+        {
+            if (!char.IsLetter(character))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
