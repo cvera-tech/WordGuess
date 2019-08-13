2547d7afbdb0b7accd410f86bedea3ed1559aa0e
jbvera <johnbenedictv@gmail.com>
2019-08-13 15:28:01 -0400

    Add GetPhrases and SelectPhrase

--- WordGuess/Program.cs
+++ WordGuess/Program.cs
@@ -11,7 +11,35 @@ namespace WordGuess
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Word Guess app!");
+            List<string> phrases = GetPhrases();
             Console.ReadKey();
         }
+
+        /// <summary>
+        /// Returns a list of phrases (strings) to be used in the game.
+        /// </summary>
+        /// <returns>The list of strings.</returns>
+        static List<string> GetPhrases()
+        {
+            var phrases = new List<string>();
+            phrases.Add("To be or not to be, that is the question");
+            phrases.Add("All the world's a stage");
+            phrases.Add("Parting is such sweet sorrow");
+            phrases.Add("To thine own self be true");
+            return phrases;
+        }
+
+        /// <summary>
+        /// Selects and returns a random phrase from a list of phrases.
+        /// </summary>
+        /// <param name="phrases">The list of phrases.</param>
+        /// <returns>A random phrase from the list.</returns>
+        static string SelectPhrase(List<string> phrases)
+        {
+            var randomizer = new Random();
+            var randomIndex = randomizer.Next(0, phrases.Count);
+            return phrases[randomIndex];
+        }
+
     }
 }
